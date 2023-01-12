77650209a892c9d9905cce620e136fc034737740
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-12 09:26:31 -0500

    add components module to pages module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,17 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 import { ContactoComponent } from './contacto/contacto.component';
-
-
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    NosotrosComponent,
-    ContactoComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, NosotrosComponent, ContactoComponent],
+  imports: [CommonModule, ComponentsModule],
 })
-export class PagesModule { }
+export class PagesModule {}
