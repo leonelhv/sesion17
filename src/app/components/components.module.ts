207ccc3cad657a84272134d9906e81526b7c5615
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-12 09:19:53 -0500

    add design navbar and navegation

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MisionComponent } from './mision/mision.component';
 import { VisionComponent } from './vision/vision.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @NgModule({
   declarations: [NavbarComponent, MisionComponent, VisionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   exports: [NavbarComponent, MisionComponent, VisionComponent],
 })
 export class ComponentsModule {}
